9f4656e0831d0c4c79e316a2ef0bfe25aa0cd305
Ahmed El Gabri <ahmed@gabri.me>
2026-03-14 22:44:15 +0100

    chore: bump version to 1.6.1

--- flake.nix
+++ flake.nix
@@ -31,7 +31,7 @@
           default = self'.packages.ccpeek;
           ccpeek = pkgs.buildGoModule {
             pname = "ccpeek";
-            version = "1.6.0";
+            version = "1.6.1";
 
             src = lib.cleanSource ./.;
 
